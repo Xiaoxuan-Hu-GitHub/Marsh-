6223d855f993004215dbe528b9e7ba4846f6b0ac
？！ <2543310139@qq.com>
2024-07-08 18:13:13 +0800

    开始编写游戏主程序。 话说你全局变量是真多啊...

---
+++ Functions/Game.cpp
@@ -0,0 +1,13 @@
+// Game.cpp - Game cycle.
+// Version: 0.0.0.0
+// Written by Xiaoxuan Hu.
+
+#include "../Modules/UI/UI.h"
+
+namespace Game_Xiaoxuan_Hu {
+	void inline game() {
+		UI_Xiaoxuan_Hu::UI output;
+		
+		return;
+	}
+}
