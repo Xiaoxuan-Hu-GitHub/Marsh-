15f4a2392fe6ac04e52cce693c188a077a0bf0dc
Xiaoxuan-Hu-GitHub <f61683ae4b1da523@qq.com>
2024-08-05 22:20:23 +0800

    修改String工具实现位置。

--- Modules/Basic/StringUtility/StringUtility.h
+++ Modules/Basic/StringUtility/StringUtility.h
@@ -1,12 +1,31 @@
 // StringUtility.h - Utilitys for strings.
-// Version: 0.0.0.2
+// Version: 0.0.0.3
 // Written by Xiaoxuan Hu.
 
 #pragma once
 
 #include <string>
+#include <sstream>
 
 namespace StringUtility_Xiaoxuan_Hu {
 	template<typename T> void stringToNumber(std::string str, T& num);
 	template<typename T> std::string numberToString(T num);
+}
+
+template<typename T> void StringUtility_Xiaoxuan_Hu::stringToNumber(std::string str, T& num) {
+	std::stringstream ss;
+
+	ss << str;
+	ss >> num;
+
+	return;
+}
+template<typename T> std::string StringUtility_Xiaoxuan_Hu::numberToString(T num) {
+	std::stringstream ss;
+	std::string str;
+
+	ss << num;
+	ss >> str;
+
+	return str;
 }
